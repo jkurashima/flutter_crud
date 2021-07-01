ee550ab0ba44b79b894d05be06444d6ddcc98d1d
Jones Kurashima <joneskurashima@gmail.com>
2021-07-01 11:56:32 -0300

    Tela inicial pronta

---
+++ lib/data/dummy_users.dart
@@ -0,0 +1,28 @@
+import 'package:flutter_crud/models/user.dart';
+
+const DUMMY_USERS = {
+  '1': const User(
+    name: 'Dart',
+    email: 'dart@starwars.com.br',
+    avatarUrl:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_3HSl6IOdiqfOZz0UCW-ajG6nmXVn8DzR-A&usqp=CAU',
+  ),
+  '2': const User(
+    name: 'Mando',
+    email: 'mando@starwars.com.br',
+    avatarUrl:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRLiHFCbpB8m7CQfiqDNw199UPP9BtUV3X-jA&usqp=CAU',
+  ),
+  '3': const User(
+    name: 'Grogo',
+    email: 'grogo@starwars.com.br',
+    avatarUrl:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_I_K-dSecI7BvXcOaGIdyEMYEptZVvVtmoQ&usqp=CAU',
+  ),
+  '4': const User(
+    name: 'BB8',
+    email: 'bb8@starwars.com.br',
+    avatarUrl:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSqxgu32W_p_wtNIomKyjNLhafM2vyNDP_y8Q&usqp=CAU',
+  ),
+};
